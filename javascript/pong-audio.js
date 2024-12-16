7d3e936c82b6a811e6f6c3e59ec07de9d0e1894c
elliemcnally <elliemcnally@my.unt.edu>
2024-12-16 16:50:40 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -57,10 +57,10 @@ soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -20; //turn down volume
 
-export var adventureMusic = new soundFile("Puppet Combo Murder House OST - Death Bells_[_YouConvert.net_].mp3");
+export var adventureMusic = new soundFile("silence.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
-ambientSound.player.volume.value = -6
+adventureMusic.player.volume.value = -16;
 
 export var villageMusic = new soundFile("silence.mp3");
 soundArray.push(villageMusic);
